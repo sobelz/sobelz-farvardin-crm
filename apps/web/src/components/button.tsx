7904e4c4163982042b69ad712b7c29a2a0d03420
ubuntu_milad <miladmxm12@gmail.com>
2026-07-14 17:31:20 +0330

    chore: add alias

---
+++ apps/web/src/components/button.tsx
@@ -0,0 +1,7 @@
+import { log } from "@/utils/log";
+
+const Button = () => {
+  return <button onClick={log}>click</button>;
+};
+
+export default Button;
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import { defineConfig } from "vite-plus";
 import react from "@vitejs/plugin-react";
 import { lazyPlugins } from "vite-plus";
@@ -5,6 +6,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   lint: {
     plugins: ["react", "typescript", "oxc"],
     rules: {
