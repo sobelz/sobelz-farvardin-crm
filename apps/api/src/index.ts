e78bac70103b0911355e7bc58f50e47f47a3d456
ubuntu_milad <miladmxm12@gmail.com>
2026-07-18 10:24:15 +0330

    chore: add alias for nodejs

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,6 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import { auth } from "./lib/auth.ts";
+import { auth } from "#/lib/auth";
 
 const app = new Hono<{
   Variables: {
